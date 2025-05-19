7aa36d4cc6979000f8780d1e2ffed6025231b61a
RedietBT <133571293+RedietBT@users.noreply.github.com>
2025-05-19 13:11:16 +0300

    connected with database and jdk version changed 23

--- springboot-backend/src/main/java/com/example/springboot_backend/controler/EmployeeControler.java
+++ springboot-backend/src/main/java/com/example/springboot_backend/controler/EmployeeControler.java
@@ -1,4 +1,32 @@
 package com.example.springboot_backend.controler;
 
+import com.example.springboot_backend.model.Employee;
+import com.example.springboot_backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeControler {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return repository.findAll();
+    }
+
+    //get employee by ID
+//    @GetMapping("{id}")
+//    public Employee getEmployeeById(@PathVariable Long id){
+//        return repository.findById(id)
+//                .orElseThrow();
+//    }
 }
