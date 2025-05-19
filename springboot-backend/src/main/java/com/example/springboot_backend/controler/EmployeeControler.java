408c39d724df1461e3c2f8d55fd7f9072d858216
RedietBT <133571293+RedietBT@users.noreply.github.com>
2025-05-19 13:29:57 +0300

    angular configeration done

--- springboot-backend/src/main/java/com/example/springboot_backend/controler/EmployeeControler.java
+++ springboot-backend/src/main/java/com/example/springboot_backend/controler/EmployeeControler.java
@@ -4,7 +4,7 @@ import com.example.springboot_backend.model.Employee;
 import com.example.springboot_backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
